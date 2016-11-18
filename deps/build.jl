fcc4f83bfd55ed35c8d186837a30e4e5b572643c
Michael Alexander Ewert <Orbots@users.noreply.github.com>
2016-11-17 19:38:46 -0800

    ehhh... should be a string

--- deps/build.jl
+++ deps/build.jl
@@ -52,7 +52,7 @@ PYTHON_PACKAGE_ROOT = string(ENV["DD_TOOLS_ROOT"],"/", ENV["DD_OS"], "/package/p
 ENV["PATH"] = string( CMAKE_PACKAGE_ROOT,"/bin:", PYTHON_PACKAGE_ROOT,"/bin:",ENV["PATH"])
 # GCC_INSTALL_PREFIX doesn't seem to be working, so we will resort to this.  
 # note, I think we'll need to set this whenever we want to use Cxx package
-if haskey( ENV, LD_LIBRARY_PATH )
+if haskey( ENV, "LD_LIBRARY_PATH" )
     ENV["LD_LIBRARY_PATH"] = string( GCC_PACKAGE_ROOT, "/lib:", GCC_PACKAGE_ROOT, "/lib64:", ENV["LD_LIBRARY_PATH"])
 else
     ENV["LD_LIBRARY_PATH"] = string( GCC_PACKAGE_ROOT, "/lib:", GCC_PACKAGE_ROOT, "/lib64")
